af67a01615956912c958331e7e7fdfe4682c5091
Nicdao, Justine A <83365551+Nodica28@users.noreply.github.com>
2022-02-24 16:00:05 +0800

    Add files via upload

---
+++ Resume Project/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [{path: 'about', component:AboutComponent},
+                        {path: 'skills', component:SkillsComponent},
+                        {path: 'portfolio', component:PortfolioComponent},
+                        {path: 'contact', component:ContactComponent},
+                        {path: 'home', component:HomeComponent},
+                        {path: '', component:HomeComponent},
+                        {path: '**', component:PagenotfoundComponent}];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
